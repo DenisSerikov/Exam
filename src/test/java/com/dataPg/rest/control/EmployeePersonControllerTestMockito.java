fea5815526e1c12b9094acf610206e6800824292
DenisSerikov <113181052+DenisSerikov@users.noreply.github.com>
2024-07-02 23:18:19 +0300

    Update EmployeePersonControllerTestMockito.java

--- src/test/java/com/dataPg/rest/control/EmployeePersonControllerTestMockito.java
+++ src/test/java/com/dataPg/rest/control/EmployeePersonControllerTestMockito.java
@@ -1,87 +1,79 @@
-package com.dataPg.rest.control;
-
-import com.dataPg.rest.RestApplication;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import com.dataPg.rest.model.Employee;
 import com.dataPg.rest.model.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest(classes = RestApplication.class)
-class EmployeePersonControllerTestMockito {
-    @Mock
+public class EmployeePersonControllerTest {
+
+    private EmployeePersonController empController;
     private RestTemplate restTemplate;
 
-    @InjectMocks
-    private EmployeePersonController empController = new EmployeePersonController();
+    @BeforeEach
+    void setup() {
+        restTemplate = new RestTemplate();
+        empController = new EmployeePersonController(restTemplate);
+    }
 
     @Test
-    @WithMockUser
     void whenGetEmployee() {
-        Mockito
-                .when(restTemplate.exchange(
-                        ArgumentMatchers.eq("http://localhost:8080/person/"),
-                        ArgumentMatchers.eq(HttpMethod.GET),
-                        ArgumentMatchers.<HttpEntity<List<Void>>>any(),
-                        ArgumentMatchers.<ParameterizedTypeReference<List<Person>>>any())
-                )
-          .thenReturn(new ResponseEntity<>(List.of(
-                  new Person(1, "person1", "person1"),
-                  new Person(2, "person2", "person2")
-          ), HttpStatus.OK));
+        // Заглушка для ответа от RestTemplate
+        ResponseEntity<List<Person>> responseEntity = new ResponseEntity<>(List.of(
+                new Person(1, "person1", "person1"),
+                new Person(2, "person2", "person2")
+        ), HttpStatus.OK);
+
+        Mockito.when(restTemplate.exchange(
+                "http://localhost:8080/person/",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Person>>() {})
+        ).thenReturn(responseEntity);
 
         ResponseEntity<Employee> resp = empController.getEmployee("token");
         assertThat(resp.getStatusCode(), is(HttpStatus.OK));
-        assertThat(resp.getBody().getName(), is("Yugay"));
+        assertThat(resp.getBody().getName(), is("Sergay")); 
         assertThat(resp.getBody().getTin(), is("9876543"));
         assertThat(resp.getBody().getAccounts().size(), is(2));
     }
 
     @Test
-    @WithMockUser
     void whenGetPersonByIdThenOk() {
-        Mockito
-                .when(restTemplate.exchange(
-                        ArgumentMatchers.eq("http://localhost:8080/person/1"),
-                        ArgumentMatchers.eq(HttpMethod.GET),
-                        ArgumentMatchers.<HttpEntity<List<Void>>>any(),
-                        ArgumentMatchers.<ParameterizedTypeReference<Person>>any())
-                )
-                .thenReturn(new ResponseEntity<>(
-                        new Person(1, "person1", "password1"),
-                        HttpStatus.OK)
-                );
+        // Заглушка для ответа от RestTemplate
+        ResponseEntity<Person> responseEntity = new ResponseEntity<>(
+                new Person(1, "person1", "password1"),
+                HttpStatus.OK);
+
+        // Мокирование RestTemplate не требуется, используем реальный объект
+        Mockito.when(restTemplate.exchange(
+                "http://localhost:8080/person/1",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Person>() {})
+        ).thenReturn(responseEntity);
 
         ResponseEntity<Person> resp = empController.getPersonById(1, "token");
         assertThat(resp.getStatusCode(), is(HttpStatus.OK));
-        assertThat(resp.getBody().getLogin(), is("person1"));
-        assertThat(resp.getBody().getPassword(), is("password1"));
+        assertThat(resp.getBody().getLogin(), is("person1")); // Замените на фактическое ожидаемое значение
+        assertThat(resp.getBody().getPassword(), is("password1")); // Замените на фактическое ожидаемое значение
     }
 
     @Test
-    @WithMockUser
     void whenGetPersonByIdThenNotFound() {
-        Mockito
-                .when(restTemplate.exchange(
-                        ArgumentMatchers.eq("http://localhost:8080/person/1"),
-                        ArgumentMatchers.eq(HttpMethod.GET),
-                        ArgumentMatchers.<HttpEntity<List<Void>>>any(),
-                        ArgumentMatchers.<ParameterizedTypeReference<Person>>any())
-                )
-                .thenThrow(HttpClientErrorException.class);
+        // Мокирование RestTemplate не требуется, используем реальный объект
+        Mockito.when(restTemplate.exchange(
+                "http://localhost:8080/person/1",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Person>() {})
+        ).thenThrow(HttpClientErrorException.class);
 
         ResponseEntity<Person> resp = empController.getPersonById(1, "token");
         assertThat(resp.getStatusCode(), is(HttpStatus.NOT_FOUND));
@@ -92,39 +84,40 @@ class EmployeePersonControllerTestMockito {
 
     @Test
     void whenCreatePerson() {
-        Mockito
-                .when(restTemplate.postForObject(
-                        ArgumentMatchers.eq("http://localhost:8080/person/sign-up"),
-                        ArgumentMatchers.<HttpEntity<List<Void>>>any(),
-                        ArgumentMatchers.eq(Person.class))
-                )
-                .thenReturn(new Person("person1", "password1"));
+        // Заглушка для ответа от RestTemplate
+        Person newPerson = new Person("person1", "password1");
+        Mockito.when(restTemplate.postForObject(
+                "http://localhost:8080/person/sign-up",
+                new HttpEntity<>(newPerson),
+                Person.class)
+        ).thenReturn(newPerson);
 
         ResponseEntity<Person> resp = empController.createPerson(new Person(), "token");
         assertThat(resp.getStatusCode(), is(HttpStatus.CREATED));
-        assertThat(resp.getBody().getLogin(), is("person1"));
-        assertThat(resp.getBody().getPassword(), is("password1"));
+        assertThat(resp.getBody().getLogin(), is("person1")); 
+        assertThat(resp.getBody().getPassword(), is("password1")); 
     }
 
     @Test
     void whenUpdatePerson() {
-        Mockito.doNothing().when(restTemplate);
+        Mockito.doNothing().when(restTemplate).put(
+                "http://localhost:8080/person/",
+                new HttpEntity<>(new Person())
+        );
+
         ResponseEntity<Void> resp = empController.updatePerson(new Person(), "token");
         assertThat(resp.getStatusCode(), is(HttpStatus.OK));
     }
 
     @Test
     void whenDeletePerson() {
-        Mockito
-                .when(restTemplate.exchange(
-                        ArgumentMatchers.eq("http://localhost:8080/person/1"),
-                        ArgumentMatchers.eq(HttpMethod.DELETE),
-                        ArgumentMatchers.<HttpEntity<List<Void>>>any(),
-                        ArgumentMatchers.<ParameterizedTypeReference<Void>>any())
-                )
-                .thenReturn(
-                        new ResponseEntity<>(HttpStatus.OK)
-                );
+        Mockito.when(restTemplate.exchange(
+                "http://localhost:8080/person/1",
+                HttpMethod.DELETE,
+                null,
+                Void.class)
+        ).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
         ResponseEntity<Void> resp = empController.deletePerson(1, "token");
         assertThat(resp.getStatusCode(), is(HttpStatus.OK));
     }
